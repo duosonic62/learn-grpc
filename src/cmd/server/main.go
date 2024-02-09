c4b21bc72f2366bfc9ade05a21418724eb7942c9
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2024-02-09 21:53:04 +0900

    add handler

--- src/cmd/server/main.go
+++ src/cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+
+	hallopb "mygrpc/pkg/grpc"
 )
 
 func main() {
@@ -17,6 +20,9 @@ func main() {
 	}
 
 	s := grpc.NewServer()
+
+	hallopb.RegisterGreetingServiceServer(s, NewMyServer())
+
 	go func() {
 		log.Printf("start gRPC server port: %v", port)
 		s.Serve(listener)
@@ -28,4 +34,18 @@ func main() {
 	<-quit
 	log.Printf("stopping gRPC server...")
 	s.GracefulStop()
+}
+
+type myServer struct {
+	hallopb.UnimplementedGreetingServiceServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hallopb.HelloRequest) (*hallopb.HelloResponse, error) {
+	return &hallopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
 }
